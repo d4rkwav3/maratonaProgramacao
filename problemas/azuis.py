cbd22095b7cfe093df9f0005acf5fcb55895bc17
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-05-16 00:43:29 -0300

    azuis.py atualizado + aritmetica.py + dardos.py

--- problemas/azuis.py
+++ problemas/azuis.py
@@ -1,45 +1,58 @@
-m = "AUG"
-f = ("UUC", "UUU")
-i = ("AUA", "AUC", "AUU")
+class RNA():
+    m = "AUG"
+    f = ("UUC", "UUU")
+    i = ("AUA", "AUC", "AUU")
 
-match1 = m + f[0] + i[0]
-match2 = m + f[0] + i[1]
-match3 = m + f[0] + i[2]
-match4 = m + f[1] + i[0]
-match5 = m + f[1] + i[1]
-match6 = m + f[1] + i[2]
+    match1 = m + f[0] + i[0]
+    match2 = m + f[0] + i[1]
+    match3 = m + f[0] + i[2]
+    match4 = m + f[1] + i[0]
+    match5 = m + f[1] + i[1]
+    match6 = m + f[1] + i[2]
 
-possible_matchs = [match1, match2, match3, match4, match5, match6]
-samples = []
-results = []
+    possible_matchs = [match1, match2, match3, match4, match5, match6]
+
+    samples = []
+    results = []
+
+    def __init__(self, samples: list):
+        self.samples = samples
+        self.search_match()
+
+    def search_match(self):
+        for each in self.samples:
+            match_found = False
+            mfi = each.find(self.m)
+            
+            if mfi >= 0:
+                subsample = each[mfi:mfi + 9]
+                if len(subsample) == 9:
+                    for match in range(len(self.possible_matchs)):
+                        if subsample == self.possible_matchs[match]:
+                            match_found = True
+                            break
+                        else:
+                            continue
+
+            if match_found:
+                self.results.append('S')
+            else:
+                self.results.append('N')
+
+    def show_results(self):
+        for test in range(len(self.results)):
+            print(self.results[test])
+        '''if test < (len(self.results) -1):
+            print(self.results[test])
+        elif test == (len(self.results) - 1):
+            print(self.results[test], end='')'''
+
+test_samples = []
 runs = int(input())
 
 for tests in range(runs):
     sample = input()
-    samples.append(sample.strip())
+    test_samples.append(sample.strip())
 
-for i in samples:
-    match_found = False
-    mfi = i.find(m)
-    
-    if mfi >= 0:
-        s = i[mfi:mfi + 9]
-        if len(s) == 9:
-            for b in range(len(possible_matchs)):
-                if s == possible_matchs[b]:
-                    match_found = True
-                    break
-                else:
-                    continue
-
-    if match_found:
-        results.append('S')
-    else:
-        results.append('N')
-
-for r in range(len(results)):
-    print(results[r])
-    '''if r < (len(results) -1):
-        print(results[r])
-    elif r == (len(results) - 1):
-        print(results[r], end='')'''
+blue_eyes = RNA(test_samples)
+blue_eyes.show_results()
