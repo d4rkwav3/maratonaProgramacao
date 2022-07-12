ed76b09b54fc2f766a4047d0b59c75f0ffe78205
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-07-11 23:45:39 -0300

    novos exercícios

---
+++ python_avançado/decorators.py
@@ -0,0 +1,45 @@
+'''
+2.4.1.7 Lab – timestamping logger
+
+Scenario
+
+    Create a function decorator that prints a timestamp (in a form like year-month-day hour:minute:seconds, eg. 2019-11-05 08:33:22)
+    Create a few ordinary functions that do some simple tasks, like adding or multiplying two numbers.
+    Apply your decorator to those functions to ensure that the time of the function executions can be monitored.
+
+# import module responsible for time processing
+from datetime import datetime
+
+# get current time using now() method
+timestamp = datetime.now()
+
+# convert timestamp to human-readable string, following passed pattern:
+string_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+print(string_timestamp)
+
+
+'''
+from datetime import datetime as dt
+
+def show_time(function):
+    def wrapper(*args, **kwargs):
+        ts = dt.now()
+        time = ts.strftime('%Y-%m-%d %H:%M:%S')
+        print('{} performed at {}'.format(function.__name__, time))
+        func = function(*args, **kwargs)
+        return func
+    return wrapper
+
+@show_time
+def soma (*args):
+    somatorio = 0
+    for i in args:
+        if type(i) == int or type(i) == float:
+            somatorio += i
+        else:
+            continue
+
+    return print(somatorio)
+
+print(soma(1, 2, 3))
