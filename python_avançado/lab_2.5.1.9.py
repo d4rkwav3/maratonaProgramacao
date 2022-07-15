ed5c97b4f7bdd914d8e6182a9386c6b389f8d24b
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-07-14 23:00:38 -0300

    Lab 2.5.1.9 finalizado

--- python_avançado/lab_2.5.1.9.py
+++ python_avançado/lab_2.5.1.9.py
@@ -1,4 +1,8 @@
 '''
+Objectives
+
+    improving the student's skills in operating with static and class methods
+
 Scenario
 
     Create a class representing a luxury watch;
@@ -20,3 +24,52 @@ before engraving the desired text, the text should be validated against restrict
     After each watch is created, call class method to see if the counter variable was increased
 '''
 
+class ExpensiveWatch:
+    __watches_created = 0
+
+    def __init__(self) -> None:
+        ExpensiveWatch.__watches_created += 1
+
+    @classmethod
+    def get_number_of_watches_created(cls):
+        return f'Number of Luxury Watches created: {cls.__watches_created}'
+
+    @classmethod
+    def add_engraving(cls, text: str):
+        if cls.validate_engraving(text):
+            _watch = cls()
+            _watch._engraved_text = text
+            return _watch
+        else:
+            raise ValueError('Invalid Text Format, use ExpensiveWatch.validate_engraving() for more information.')
+
+    @staticmethod
+    def validate_engraving(text: str) -> bool:
+        if type(text) is not str:
+            return False
+
+        elif len(text) > 40:
+            raise ValueError('Engraving text too long! Max lenght is 40 characters.')
+        
+        else:
+            special_chars = 0
+            for ch in text:
+                if ch.isalnum():
+                    continue
+                else:
+                    special_chars += 1
+
+            if special_chars > 0:
+                raise ValueError('Special characters not allowed! Use alphanumerical characters only.')
+            else:
+                return True
+
+watch1 = ExpensiveWatch()
+watch2 = ExpensiveWatch.add_engraving("PythonIsEasyToLearn")
+
+try:
+    watch3 = ExpensiveWatch.add_engraving("Python is cool but so is Java!")
+except ValueError:
+    pass
+
+print(ExpensiveWatch.get_number_of_watches_created())
