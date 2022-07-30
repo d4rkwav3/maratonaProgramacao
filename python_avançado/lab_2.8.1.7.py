f30e56373aab05b83f85c3d04d708e9d9e824b54
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-07-30 00:20:34 -0300

    Lab Composition finished

--- python_avançado/lab_2.8.1.7.py
+++ python_avançado/lab_2.8.1.7.py
@@ -25,23 +25,57 @@ Your task is to :
 
 class Tires:
     def __init__(self, size: int) -> None:
-        self.size = size
-    
-    def get_pressure(self, ready):
-        if type(ready) == str and ready == "pump":
+        self.size: int = size
+        self.pressure: float = 0
+
+    def get_pressure(self) -> None:
+        if self.pressure >= 12:
             print("Pressure is good!")
         else:
             print("Not looking good! Check your tires before use!")
 
-    def pump(self):
-        print ("Tires pumped! Ready for use.")
+    def pump(self, pressure) -> None:
+       self.pressure = pressure 
 
 class Engine:
-    def __init__(self, fuel, type) -> None:
-        self.fuel = fuel
-        self.type = type
+    def __init__(self, fuel_type) -> None:
+        self.fuel: str = fuel_type
+        self.engine_on: bool = False
 
-# pneu = Tires(15)
-# print(type(pneu.pump))
-# print(pneu.pump.__name__)
-# pneu.get_pressure(pneu.pump.__name__)
+    def start(self):
+        self.engine_on = True
+
+    def stop(self):
+        self.engine_on = False
+
+    def get_state(self):
+        if self.engine_on:
+            print("The engine is on.")
+        else:
+            print("The engine is off.")
+
+class Vehicle:
+    def __init__(self, VIN, engine, tires):
+        self.VIN: str = VIN #vehicle identification number
+        self.engine = engine
+        self.tires = tires
+
+#tires
+city_tires = Tires(15)
+offroad_tires = Tires(18)
+#engines
+electric_engine = Engine("Electric")
+petrol_engine = Engine("Combustion")
+#cars
+city_car = Vehicle("PY256CT", electric_engine, city_tires)
+off_track = Vehicle("PY512OR", petrol_engine, offroad_tires)
+
+city_car.tires.pump(10)
+off_track.tires.pump(13)
+city_car.tires.get_pressure()
+off_track.tires.get_pressure()
+
+city_car.engine.start()
+off_track.engine.stop()
+city_car.engine.get_state()
+off_track.engine.get_state()
